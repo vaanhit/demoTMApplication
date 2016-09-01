65b97f0052eea749d942736662fb4fe671d8c01f
rohitdec01 <rohitdec01@gmail.com>
2016-09-01 16:39:22 +0530

    first commit --  Basic Spring application configuration

---
+++ src/main/java/com/att/demo/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.att.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Rohit
+ *
+ */
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String index(Model model) {
+		System.out.println("Inside Controller");
+
+		return "";
+	}
+}
---
+++ target/demoTMApplication/index.jsp
@@ -0,0 +1,3 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!--  Redirect default index file on the home page. -->
+<c:redirect url="/index.html"/>
